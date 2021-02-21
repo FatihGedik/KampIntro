b0a1b6f37dadc7dc4bd84e126540f87a7ebcaef4
FatihGedik <fgedik06@gmail.com>
2021-02-21 03:32:02 +0300

    16 Ocak Ödevi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using ClassMetotDemo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine($"{musteri.Ad} {musteri.Soyad} added.");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine($"Id: {musteri.Id}");
+            Console.WriteLine($"Ad: {musteri.Ad}");
+            Console.WriteLine($"Soyad: {musteri.Soyad}");
+            Console.WriteLine($"Yaş: {musteri.Yas}");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine($"{musteri.Ad} {musteri.Soyad} deleted.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 001;
+            musteri1.Ad = "Fatih";
+            musteri1.Soyad = "Gedik";
+            musteri1.Yas = 26;
+
+            MusteriManager musmanager = new MusteriManager();
+
+            musmanager.Ekle(musteri1);
+            musmanager.Listele(musteri1);
+            musmanager.Sil(musteri1);
+
+        }
+    }
+}
